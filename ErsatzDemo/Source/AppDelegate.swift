c7c52ca88412b413bf655dbb0c05f978704d71fa
Robin Hill <rehill012@gmail.com>
2022-05-27 21:22:11 -0400

    Move files into some new folders

--- ErsatzDemo/Source/AppDelegate.swift
+++ ErsatzDemo/Source/AppDelegate.swift
@@ -1,17 +1,11 @@
-//
-//  AppDelegate.swift
-//  DemoApp
-//
 //  Created by Robin Hill on 3/14/22.
 //  Copyright © 2022 Apple. All rights reserved.
-//
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 }
---
+++ ErsatzDemo/Source/ViewController.swift
@@ -0,0 +1,7 @@
+//  Created by Robin Hill on 3/14/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {}
