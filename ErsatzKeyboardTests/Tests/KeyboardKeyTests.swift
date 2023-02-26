462ed91c56e0dee503472e72900e563a8c1898e0
Robin Hill <rehill012@gmail.com>
2023-02-25 21:52:56 -0500

    Add dark mode KeyboardKey snapshot tests

--- ErsatzKeyboardTests/Tests/KeyboardKeyTests.swift
+++ ErsatzKeyboardTests/Tests/KeyboardKeyTests.swift
@@ -15,79 +15,115 @@ class KeyboardKeyTests: XCTestCase {
         let characters = ["a", "g", "h", "ç", "ñ", "ồ", "A", "Ç", "Ñ", "Ồ", "馬"]
         
         for character in characters {
-            let keyView = ImageKey()
-            keyView.text = character
-            keyView.frame = keyRect
-            
-            let capital = (character.lowercased() != character)
-            let suffix = capital ? "_cap" : ""
-            let testName = "key_\(character)" + suffix
-            assertSnapshot(matching: keyView, as: .image, named: testName)
+            for darkMode in [false, true] {
+                let keyView = ImageKey()
+                keyView.text = character
+                keyView.colors = .character(GlobalColors.self, darkMode: darkMode)
+                keyView.frame = keyRect
+                
+                let capital = (character.lowercased() != character)
+                let capSuffix = capital ? "_cap" : ""
+                let colorSuffix = darkMode ? "_dark" : "_light"
+                let testName = "key_\(character)" + capSuffix + colorSuffix
+                assertSnapshot(matching: keyView, as: .image, named: testName)
+            }
         }
     }
     
     /// Displays a tapped keyboard key, showing its popup
     /// Note: Shadows cannot currently be rendered in snapshot tests without a host application
     func testTappedKey() {
-        let keyDelegate = MockKeyboardKeyDelegate()
-        let keyView = ImageKey()
-        keyView.text = "A"
-        keyView.delegate = keyDelegate
-        
-        let containerView = UIView(frame:
-            CGRect(
-                x: 0,
-                y: 0,
-                width: max(keyRect.width, popupSize.width),
-                height: 112
+        for darkMode in [false, true] {
+            let keyDelegate = MockKeyboardKeyDelegate()
+            let keyView = ImageKey()
+            keyView.text = "A"
+            keyView.colors = .character(GlobalColors.self, darkMode: darkMode)
+            keyView.delegate = keyDelegate
+            
+            let containerView = UIView(frame:
+                CGRect(
+                    x: 0,
+                    y: 0,
+                    width: max(keyRect.width, popupSize.width),
+                    height: 112
+                )
             )
-        )
-        containerView.backgroundColor = .lightGray
-        containerView.addSubview(keyView)
-        
-        keyView.frame = CGRect(
-            x: (containerView.bounds.width - keyRect.width) / 2,
-            y: containerView.bounds.height - keyRect.height,
-            width: keyRect.width,
-            height: keyRect.height
-        )
-        keyView.showPopup()
+            containerView.backgroundColor = .lightGray
+            containerView.addSubview(keyView)
+            
+            keyView.frame = CGRect(
+                x: (containerView.bounds.width - keyRect.width) / 2,
+                y: containerView.bounds.height - keyRect.height,
+                width: keyRect.width,
+                height: keyRect.height
+            )
+            keyView.showPopup()
 
-        let testName = "key_tapped"
-        assertSnapshot(matching: containerView, as: .image, named: testName)
+            let colorSuffix = darkMode ? "_dark" : "_light"
+            let testName = "key_tapped" + colorSuffix
+            assertSnapshot(matching: containerView, as: .image, named: testName)
+        }
     }
     
     /// Displays a single key, showing a shape
     func testShapeKey() {
-        let shapeNamePairs: [(String, Shape)] = [
-            ("shift", ShiftShape()),
-            ("backspace", BackspaceShape()),
-            ("globe", GlobeShape())
+        let shapeInfo: [(String, Shape, KeyboardKeyColors, KeyboardKeyColors)] = [
+            (
+                "shift",
+                ShiftShape(),
+                .shift(GlobalColors.self, darkMode: false, solidColorMode: false),
+                .shift(GlobalColors.self, darkMode: true, solidColorMode: false)
+            ),
+            (
+                "backspace",
+                BackspaceShape(),
+                .backspace(GlobalColors.self, darkMode: false, solidColorMode: false),
+                .backspace(GlobalColors.self, darkMode: true, solidColorMode: false)
+            ),
+            (
+                "globe",
+                GlobeShape(),
+                .action(GlobalColors.self, darkMode: false, solidColorMode: false),
+                .action(GlobalColors.self, darkMode: true, solidColorMode: false)
+            )
         ]
         
-        for (name, shape) in shapeNamePairs {
-            let keyView = ImageKey()
-            keyView.shape = shape
-            keyView.frame = keyRect
-            
-            let testName = "key_\(name)"
-            assertSnapshot(matching: keyView, as: .image, named: testName)
+        for (name, shape, lightColors, darkColors) in shapeInfo {
+            for darkMode in [false, true] {
+                let keyView = ImageKey()
+                keyView.shape = shape
+                keyView.colors = darkMode ? darkColors : lightColors
+                keyView.frame = keyRect
+                
+                let colorSuffix = darkMode ? "_dark" : "_light"
+                let testName = "key_\(name)" + colorSuffix
+                assertSnapshot(matching: keyView, as: .image, named: testName)
+            }
         }
     }
 
     /// Displays a single key, showing an image
     func testImageKey() {
-        let imageNamePairs = [
-            ("settings", UIImage.gear)
+        let imageInfo: [(String, UIImage, KeyboardKeyColors, KeyboardKeyColors)] = [
+            (
+                "settings",
+                UIImage.gear!,
+                .action(GlobalColors.self, darkMode: false, solidColorMode: false),
+                .action(GlobalColors.self, darkMode: true, solidColorMode: false)
+            )
         ]
         
-        for (name, image) in imageNamePairs {
-            let keyView = ImageKey()
-            keyView.image = UIImageView(image: image)
-            keyView.frame = keyRect
-            
-            let testName = "key_\(name)"
-            assertSnapshot(matching: keyView, as: .image, named: testName)
+        for (name, image, lightColors, darkColors) in imageInfo {
+            for darkMode in [false, true] {
+                let keyView = ImageKey()
+                keyView.image = UIImageView(image: image)
+                keyView.colors = darkMode ? darkColors : lightColors
+                keyView.frame = keyRect
+                
+                let colorSuffix = darkMode ? "_dark" : "_light"
+                let testName = "key_\(name)" + colorSuffix
+                assertSnapshot(matching: keyView, as: .image, named: testName)
+            }
         }
     }
 }
